b4886bc1d8bd0eeb3ece7a5f99393eb5f575471a
Sarabjeet Singh <ss4039417@gmail.com>
2026-05-12 03:41:54 +0530

    added vision

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { geistMono, poppins } from "../utils/fonts";
@@ -57,6 +58,16 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <Analytics />
         <SpeedInsights />
       </div>
+
+      <Script id="microsoft-clarity" strategy="afterInteractive">
+        {`
+          (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "wpn5huirly");
+        `}
+      </Script>
     </>
   );
 }
